4f624cf619fb7ffadce20f5e95f7f3eaf7873fbb
Abdurrahman <abdurrahman.maged@gmail.com>
2025-06-22 13:40:03 +0100

    Fix Path Alias Configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from "@vitejs/plugin-react";
 import tailwind from "tailwindcss";
 import { defineConfig } from "vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,6 +10,11 @@ export default defineConfig({
   css: {
     postcss: {
       plugins: [tailwind()],
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src/components/ui"),
     },
   },
 });
