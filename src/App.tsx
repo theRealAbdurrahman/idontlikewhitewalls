963ccb8fbbe281271fde798c21dfbf7afec2f565
Abdurrahman <abdurrahman.maged@gmail.com>
2025-06-27 19:51:56 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,6 @@ import { UserProfile } from "./screens/UserProfile";
 import { ProfilePage } from "./screens/ProfilePage";
 import { Chat } from "./screens/Chat";
 import { SuggestFeature } from "./screens/SuggestFeature";
-import { SignupFlow } from "./screens/SignupFlow";
 import { Callback } from "./screens/Callback";
 import { AuthProvider } from "./contexts/AuthContext";
 
@@ -29,6 +28,10 @@ const config: LogtoConfig = {
 
 export const App = (): JSX.Element => {
   return (
+    <LogtoProvider config={config}>
+      {/* bolt is fucking stupid and made the questions added to the state in the auth provider */}
+      {/* for now I am leaving it here, then later we need to refactor this */}
+      {/* by refactor this I mean refactor everything */}
       <AuthProvider>
         <Layout>
           <Routes>
@@ -47,12 +50,11 @@ export const App = (): JSX.Element => {
             <Route path="/create-community" element={<CreateCommunity />} />
             <Route path="/communities" element={<Communities />} />
             <Route path="/suggest-feature" element={<SuggestFeature />} />
-            <Route path="/signup" element={<SignupFlow />} />
             <Route path="/callback" element={<Callback />} />
             <Route path="*" element={<Navigate to="/home" replace />} />
           </Routes>
         </Layout>
       </AuthProvider>
-   
+    </LogtoProvider>
   );
 };
